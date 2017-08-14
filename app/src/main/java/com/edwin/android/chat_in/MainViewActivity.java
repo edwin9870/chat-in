191140208cb5258f2fdb96091208c1eb5183b157
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-14 17:48:18 -0400

    Set MainView to activity

--- app/src/main/java/com/edwin/android/chat_in/MainViewActivity.java
+++ app/src/main/java/com/edwin/android/chat_in/MainViewActivity.java
@@ -1,25 +1,29 @@
 package com.edwin.android.chat_in;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainViewActivity extends AppCompatActivity {
 
-    private MainViewFragment fragment;
+
+    public static final String TAG = MainViewActivity.class.getSimpleName();
+    @BindView(R.id.toolbar_main_view)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
+        ButterKnife.bind(this);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragment = MainViewFragment.newInstance();
-
-        fragmentTransaction.add(R.id.fragment_movie_view, fragment);
-        fragmentTransaction.commit();
+        Log.d(TAG, "mToolbar: " + mToolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(getString(R.string.app_name).toUpperCase());
 
 
     }
