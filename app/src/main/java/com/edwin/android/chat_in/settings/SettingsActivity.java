aed41c63736bc5f6287fe0c19ce41472f5814850
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-21 21:53:14 -0400

    Update Android support version

---
+++ app/src/main/java/com/edwin/android/chat_in/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.edwin.android.chat_in.settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edwin.android.chat_in.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/com/edwin/android/chat_in/settings/SettingsFragment.java
@@ -0,0 +1,38 @@
+package com.edwin.android.chat_in.settings;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edwin.android.chat_in.R;
+
+public class SettingsFragment extends Fragment {
+
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
