ca19ce1e20c42ec81dda9e9add52ff211d14063f
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-08-23 10:29:50 -0400

    Add Chatin contract

---
+++ app/src/main/java/com/edwin/android/chat_in/data/ChatInContract.java
@@ -0,0 +1,38 @@
+package com.edwin.android.chat_in.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Edwin Ramirez Ventura on 8/23/2017.
+ */
+
+public class ChatInContract {
+
+    public static final String AUTHORITY = "com.edwin.android.chat_in";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH_CONTACT = "CONTACT";
+    public static final String PATH_CONVERSATION = "CONVERSATION";
+
+    public static class ContactEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_CONTACT).build();
+
+        public static final String TABLE_NAME = "CONTACT";
+        public static final String COLUMN_NAME_NAME = "NAME";
+        public static final String COLUMN_NAME_NUMBER = "NUMBER";
+        public static final String COLUMN_NAME_PROFILE_IMAGE_PATH = "PROFILE_IMAGE_PATH";
+    }
+
+    public static class ConversationEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_CONVERSATION).build();
+
+        public static final String TABLE_NAME = "CONVERSATION";
+        public static final String COLUMN_NAME_DATE = "DATE";
+        public static final String COLUMN_NAME_MESSAGE = "MESSAGE";
+        public static final String COLUMN_NAME_FROM = "FROM";
+        public static final String COLUMN_NAME_TO = "TO";
+    }
+}
