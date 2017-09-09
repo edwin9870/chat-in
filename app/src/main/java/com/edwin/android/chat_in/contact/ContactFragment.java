71936970c5c003c1ea51e11a9b1a06b5322f8c0d
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-09-09 12:07:31 -0400

    Remote vibrate and souns settings

--- app/src/main/java/com/edwin/android/chat_in/contact/ContactFragment.java
+++ app/src/main/java/com/edwin/android/chat_in/contact/ContactFragment.java
@@ -67,7 +67,6 @@ public class ContactFragment extends Fragment implements ContactListener, Contac
                 .getActivity(), getResources().getInteger(R.integer.space_between_chat_list))));
         mPresenter.getContacts();
 
-
         return view;
     }
 
